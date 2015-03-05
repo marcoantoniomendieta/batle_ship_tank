bdb8148a8ee5dee0f00a3b1960e57a0ff3cae127
Marco Mendieta <mbecerra@pros.com>
2015-03-05 18:03:43 -0400

    Adding last code written by Leo

--- README.md
+++
@@ -1,1 +0,0 @@
-# batle_ship_tank
--- js/main.js
+++ js/main.js
@@ -1,24 +1,5 @@
-var setMatriz = function(num){
-	var matriz = new Array();
-	for (var i = 0; i < num; i++){
-			matriz[i] = new Array();
-		for (var j = 0; j < num; j++){
-			matriz[i][j]= 'a';
-			continue;
-		};
-	};
-};
+// main js file
 
+var bsg = new Game();
 
-var printMatrizOne = function(){
-	var matriz = new Array();
-	matriz[0] = new Array ('e00', 'e01', 'e02'); 
-	matriz[1] = new Array ('e10', 'e11', 'e12'); 
-	matriz[2] = new Array ('e20', 'e21', 'e22'); 
-	for (var i = 0; i < matriz.length; i++){
-		for (var j = 0; j < matriz[i].length; j++){
-			console.log(matriz[i][j]);
-			continue;
-		};
-	};
-};
+bsg.start(1);
